2677877262f80544fe8c3c4766396a3eb2841308
ajstrg <93103930+ajstrg@users.noreply.github.com>
2023-02-01 23:02:53 +0800

    Create Gym.java

---
+++ Gym.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Gym extends Location {
+
+  // pingpong
+  // mood += 10
+  // stress += 10
+  // energy -= 25
+
+  // dance
+  // mood += 20
+  // stress -= 15
+  // energy -= 20
+
+
+}
