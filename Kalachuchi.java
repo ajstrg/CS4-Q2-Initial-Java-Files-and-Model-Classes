04363ff13f6d553769c379546a91b39a32fa0582
ajstrg <93103930+ajstrg@users.noreply.github.com>
2023-02-01 23:03:14 +0800

    Create Kalachuchi.java

---
+++ Kalachuchi.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Kalachuchi extends Location {
+
+  // chill with friends
+  // mood += 20
+  // stress -= 20
+  // energy -= 5
+
+  // nap
+  // mood += 10
+  // stress -= 10
+  // energy += 15
+
+}
