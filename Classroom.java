50c97e2e8c7aca36ee0c61fa168ba09153ed3fa0
ajstrg <93103930+ajstrg@users.noreply.github.com>
2023-02-01 23:00:34 +0800

    Create Classroom.java

---
+++ Classroom.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Classroom extends SHB {
+
+  private String subject;
+
+    public String getSubject(){
+      return subject;
+    }
+
+    // chem
+    // mood -= 20;
+    // stress += 25;
+    // energy -= 30;
+
+    // research
+    // mood -= 15;
+    // stress += 30;
+    // energy -= 35;
+
+    // valed
+    // mood += 10;
+    // stress += 2;
+    // energy -= 5;
+
+}
