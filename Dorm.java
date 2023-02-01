2cabe0e6709836cb86332e100ed055461b89b179
ajstrg <93103930+ajstrg@users.noreply.github.com>
2023-02-01 23:00:49 +0800

    Create Dorm.java

---
+++ Dorm.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Dorm extends Location {
+
+  // sleep level is calculated as percentage out of 8 hours
+  // mood += 15 per hour
+  // stress -= 15 per hour
+  // energy += 20 per hour
+  
+}
