03c3bc41adc24c025c7c075aeeb06ebd251ee9af
ajstrg <93103930+ajstrg@users.noreply.github.com>
2023-02-01 23:01:01 +0800

    Create Field.java

---
+++ Field.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Field extends Location {
+
+  // frisbee
+  // mood += 15
+  // stress -= 20
+  // energy -= 20
+
+  // soccer
+  // mood += 10
+  // stress -= 10
+  // energy -= 30
+
+  // badminton
+  // mood += 20
+  // stress -= 15
+  // energy -= 20
+  
+}
