c93720a9e91e65580e5712d254ac381bb07feec5
ajstrg <93103930+ajstrg@users.noreply.github.com>
2023-02-01 23:03:40 +0800

    Create Likes.java

---
+++ Likes.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Likes extends Person {
+
+  private String favLoc, favSub, favFood;
+
+    public String getFaveLoc(){
+      return favLoc;
+    }
+
+    public String getFaveSub(){
+      return favSub;
+    }
+
+    public String getFaveFood(){
+      return favFood;
+    }
+}
